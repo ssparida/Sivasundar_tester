a224e45e6fa65d56a05300d1cf434bb4fddc6be1
ssparida <ssparida9@gmail.com>
2022-09-02 06:11:00 +0000

    commited in cell test

--- tst.java
+++ tst.java
@@ -1,2 +1,4 @@
 test cases and scenerio are written
 cellprop has changed the test suites and write all the codes
+and siva sundar who is a automation tester has done all the things make new directory
+
